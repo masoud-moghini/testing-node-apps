c9a0741e9f20fc7207dd76b0e90c78e3593f60dd
Masoud Moghini <koosha_pooyeshgar@outlook.com>
2017-08-23 00:40:55 +0430

    testing express

--- util/util.js
+++ util/util.js
@@ -5,3 +5,17 @@ module.exports.add=(a,b)=>{
 module.exports.square=(x)=>{
     return x*x ;
 }
+
+
+module.exports.squareAdd=(x,callback)=>{
+    setTimeout(()=>{
+        callback(x*x);
+        },1500)
+}
+module.exports.setName=(user,name)=>{
+    var names=name.split(' ');
+    user.first_name=names[0];
+    user.second_name=names[1];
+
+    return ; 
+}
--- util/util.test.js
+++ util/util.test.js
@@ -4,16 +4,31 @@ const expect=require('expect')
 it('should add two numbers',()=>{
     var res=test_module.add(5,19);
 
-    expect(res).toBe(25);
+    expect(res).toBe(24);
     // if(res!==24){
     //     throw new Error(`must be 24 but got ${res}`);
     // }
 });
 
 
+it('should do asynchronous square',(done)=>{
+    test_module.squareAdd(5,(square)=>{
+        expect(square).toBe(25).toBeA('number');
+        done();
+    })
+})
+it('should verify ',()=>{
+    var user=new Object();
+    test_module.setName(user,'masoud moghini');
+    expect(user).toInclude({first_name:'masoud',second_name:'moghini'});
+})
+
 it('should return square of a number',()=>{
     var res=test_module.square(7);
-    if(res!==49){
-        throw new Error(`must be 49 but got ${res}`);
-    }
+    // if(res!==49){
+    //     throw new Error(`must be 49 but got ${res}`);
+    // }
+
+    expect(res).toBe(49);
+    expect(res).toBeA('number');
 });
